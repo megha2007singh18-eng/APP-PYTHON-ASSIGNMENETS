59ad9fefeb0b8ad8477941b694c89eb5268febd4
megha2007singh18-eng <megha2007singh18@gmail.com>
2026-09-21 09:28:04 +0530

    exp-6 knapsack.py
    Implement bottom-up dynamic programming solution for knapsack problem.

---
+++ exp-6 knapsack.py
@@ -0,0 +1,23 @@
+def knapsack_bottom_up(values, weights, W):
+    n = len(values)
+
+    dp = [[0 for _ in range(W + 1)] for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        for w in range(W + 1):
+
+            if weights[i - 1] <= w:
+                dp[i][w] = max(
+                    dp[i - 1][w],
+                    dp[i - 1][w - weights[i - 1]] + values[i - 1]
+                )
+            else:
+                dp[i][w] = dp[i - 1][w]
+
+    return dp[n][W]
+
+values = [60, 100, 120]
+weights = [10, 20, 30]
+W = 50
+
+print(knapsack_bottom_up(values, weights, W))
